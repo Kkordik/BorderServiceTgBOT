d9ffce6340a36d185d67e27a7b4d50100dda621a
kkordik <99617240+Kkordik@users.noreply.github.com>
2023-06-10 08:05:45 +0200

    Chat join handler

--- updates_handling/new_chat_memb.py
+++ updates_handling/new_chat_memb.py
@@ -3,6 +3,11 @@ import aiogram.types
 from run_bot import dp
 
 
-def sdfsdfs(update: aiogram.types.Update):
+def new_chat_member(message: aiogram.types.Message):
     print(update.__dict__)
+
+
+def register_new_member_handler(dp: Dispatcher):
+    dp.register_message_handler(new_chat_member, content_types=types.ContentType.NEW_CHAT_MEMBERS)
+
 
